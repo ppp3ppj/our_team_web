fb9bd76c256af8d2db0943ba84c3358b40d40136
ppp3ppj <ppp.3ppj@gmail.com>
2025-01-08 23:29:28 +0700

    add: port 8080 in develop config in lua

--- config.lua
+++ config.lua
@@ -1,7 +1,8 @@
 local config = require("lapis.config")
 
 config("development", {
-  server = "nginx",
-  code_cache = "off",
-  num_workers = "1"
+    server = "nginx",
+    code_cache = "off",
+    num_workers = "1",
+    port = 8080
 })
