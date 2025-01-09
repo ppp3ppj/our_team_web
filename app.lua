f92ec461f30f1a810be3a65044de106c9c0ec2c1
ppp3ppj <ppp.3ppj@gmail.com>
2025-01-09 22:20:27 +0700

    add: add routes and enable etlua and load layout

--- app.lua
+++ app.lua
@@ -1,8 +1,16 @@
 local lapis = require("lapis")
 local app = lapis.Application()
+app:enable("etlua") -- enable etlua
+app.layout = require "views.layout"
 
+
+-- Routes --
 app:get("/", function()
   return "Welcome to Lapis " .. require("lapis.version")
+end)
+
+app:get("/me", function()
+    return { render = "pages.index" }
 end)
 
 return app
